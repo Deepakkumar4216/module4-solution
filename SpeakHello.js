be2b97cc8d39340d2cdcdf09a6c883cb72120f3d
unknown <deepakmrga75@gmail.com>
2020-06-09 23:44:11 +0530

    updated.

--- SpeakHello.js
+++ SpeakHello.js
@@ -2,23 +2,27 @@
 // See Lecture 52, part 2
 (function (window) {
 	
-}
+
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = {};
-helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var helloSpeaker = new Object();
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
+
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function helloSpeaker(names) {
-  console.log(speakWord + " " + names);
-});
+helloSpeaker.speak = function(name) {
+	// body...
+
+  console.log(speakWord + " " + name);
+};
+window.helloSpeaker = helloSpeaker;
+})(window);
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
